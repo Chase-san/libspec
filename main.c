6d71ef9e72591e2c614358c86f9aaefff59c5397
Chase <chasesan@gmail.com>
2014-03-25 05:49:58 -0400

    main.c setup for testing

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "types.h"
 #include "libspec.h"
 
 void *load_file(char *filename, size_t size) {
@@ -9,6 +12,19 @@ void *load_file(char *filename, size_t size) {
 	return (uint8_t *)ptr;
 }
 
+void write_file(char *filename, void *ptr, size_t size) {
+	FILE *f = fopen(filename, "wb");
+	fwrite(ptr, size, 1, f);
+	fclose(f);
+}
+
+void ucs2_to_ascii(char *dst, char16_t *src, size_t size) {
+	for(size_t i = 0; i < size; ++i) {
+		dst[i] = (char)src[i];
+	}
+}
+
 int main() {
+
 	return 0;
 }
